1f5cab9e1a662d7b01e9013db63a3dcfa96abaaf
Razin <razinm270@gmail.com>
2025-08-16 00:05:33 +0530

    Corrected issue in obtaining COM port

--- RA-08H/basic_code/basic_code.ino
+++ RA-08H/basic_code/basic_code.ino
@@ -1,9 +1,18 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  Serial1.setTX(0); // TX(2040) -> TX(LR1262)
+  Serial1.setRX(1); // RX(2040) -> TX(LR1262) 
+  Serial1.begin(9600);
+  //Raspeberry Pi Pico board should be used along with new version of board manager
+  //Set Serial monitor baud 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(Serial.available()) {
+    Serial1.write(Serial.read());
+  }
+  if(Serial1.available()){
+    Serial.write(Serial1.read());
+  }
 
 }
